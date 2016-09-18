5f5ea9edc229e60fba8b357a1173f46e9c6f9e86
Ryan Polsky <ryanpolsky@d-172-25-196-103.dhcp.virginia.edu>
2016-09-18 17:07:50 -0400

    Initial Commit

---
+++ Speech Feedback/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speech Feedback
+//
+//  Created by Ryan Polsky on 9/18/16.
+//  Copyright © 2016 Ryan Polsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Speech FeedbackTests/Speech_FeedbackTests.swift
@@ -0,0 +1,36 @@
+//
+//  Speech_FeedbackTests.swift
+//  Speech FeedbackTests
+//
+//  Created by Ryan Polsky on 9/18/16.
+//  Copyright © 2016 Ryan Polsky. All rights reserved.
+//
+
+import XCTest
+@testable import Speech_Feedback
+
+class Speech_FeedbackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
