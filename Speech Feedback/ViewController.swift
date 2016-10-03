0ea7acc807f2ae4b6c855ecf622ac646901c0ced
Ryan Polsky <ryanpolsky@ryans-mbp-2.home>
2016-10-02 22:56:15 -0400

    Version 3: Basic Functionality, Graph Display and Comment Loop

--- Speech Feedback/ViewController.swift
+++ Speech Feedback/ViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController, ViewController2Delegate {
+class ViewController: UIViewController, ViewController2Delegate, UITableViewDelegate, UITableViewDataSource {
     
+    //Temporary hard-coded posts and posters, later data will come form users
+    let posters = ["Poster a","Poster b","Poster c","Poster d"]
+    let posts = ["Post a","Post b","Post c","Post d"]
+    var postCounter = 0
     var timer = Timer()
+    //Timer value
     var count = 0
-    //var passedDictionary: NSMutableDictionary = [:]
-    //var passedCount = 0
-    
+    //Maps time to slider value
     var dictionary = NSMutableDictionary()
-    
+    //Displays timer time
     @IBOutlet weak var label: UILabel!
+    //Displays comment loop
+    @IBOutlet weak var tableView: UITableView!
        
     //Rotate slider 90 degrees
     @IBOutlet weak var slider: UISlider! {
@@ -31,16 +36,17 @@ class ViewController: UIViewController, ViewController2Delegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
     @IBAction func valueChanged(_ sender: AnyObject) {
-        //snaps slider to interger values 
+    //snaps slider to interger values
        let intValue = (Int) (slider.value)
         let rounded = Float(intValue)
         slider.setValue( rounded, animated: true)
@@ -58,6 +64,16 @@ class ViewController: UIViewController, ViewController2Delegate {
         dictionary[count] = slider.value
         count+=1
         label.text =  String(count)
+        //every 5 seconds, we increment commentCounter to display next item in the array
+        if(count%5 == 0){
+            postCounter+=1
+        }
+        //Once commentCounter is larger then the number of elements in the array, we rest to 0
+        if(postCounter == posts.count){
+            postCounter = 0
+        }
+        tableView.reloadData()
+        
         
     }
     //Segues to ViewController2
@@ -67,8 +83,6 @@ class ViewController: UIViewController, ViewController2Delegate {
         for index in 0...count {
             print( "\(index) : \(dictionary[index]) ")
         }
-            
-        
     }
     
     //sets up this instance of ViewController as the delegate member of the
@@ -78,10 +92,33 @@ class ViewController: UIViewController, ViewController2Delegate {
             let dest = segue.destination as! ViewController2
             dest.delegate = self
             
-            
-          
-            
         }
     }
+    
+    //The following methods allow ViewController to conform to the UITableViewDelegate
+    //and UITableViewDataSource protocols
+
+    //Only want one section
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "commentLoop", for: cellForRowAt)
+        cell.textLabel?.text = posters[postCounter]
+        cell.detailTextLabel?.text = posts[postCounter]
+        return cell
+        
+    }
+    
+    //We only want one row
+    func tableView(_: UITableView, numberOfRowsInSection: Int) -> Int{
+        return 1
+    }
+    
+
+    
+    
+    
 }
 
